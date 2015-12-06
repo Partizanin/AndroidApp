0f23e2a2ceff669d39f0cdbb6d40abf64ae502a4
Partizanin <topiciv1@yandex.ua>
2015-12-06 19:42:10 +0200

    working version  add ember dependencies

---
+++ web/javascript/app.js
@@ -0,0 +1,19 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+  // put your routes here
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model: function() {
+    return data;
+  }
+});
+
+var data = {"question": "Родова організація суспільства, в якій провідну роль відігравав чоловік, — це:",
+"answers":[
+  "А Лимарство",
+  "Б Конярство",
+  "В Вівчарство",
+  "Г Скотарство"
+  ]};
