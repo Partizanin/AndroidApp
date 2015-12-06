8ea8cb156ac3e205c255d87b8e80c695ddd35e30
Partizanin <topiciv1@yandex.ua>
2015-12-06 14:46:17 +0200

    move script source to file

--- web/javascript/js1.js
+++ web/javascript/js1.js
@@ -1,101 +1,19 @@
-/**
- * Created by Partizanin on 06.12.2015.
- */
 
 $(window).load(function () {
     loadData();
-
 });
 
-$("#test").click(function () {
+$("#Save").click(function () {
     saveData();
 
 });
 
-function loadData() {
-    var data = JSON.parse(localStorage.getItem("answers"));
-    var value = data.paragraphs[1].tasks[1].answers;
+$("#Reload").click(function () {
+    location.reload();
+});
 
-    $.each(value, function(key, value) {
-        console.log(key, value);
-    });
+function  saveData(){
 
-    var answer1 = $('#lab1').text();
-    var answer2 = $('#lab2').text();
-    var answer3 = $('#lab3').text();
-    var answer4 = $('#lab4').text();
-
-    $.each(value, function(key, value) {
-        if(answer1 == key){
-            $('#check1').prop('checked', value);
-        }
-        if(answer2 == key){
-            $('#check2').prop('checked', value);
-        }
-        if(answer3 == key){
-            $('#check3').prop('checked', value);
-        }
-        if(answer4 == key){
-            $('#check4').prop('checked', value);
-        }
-    });
-
-}
-
-
-function saveObject(paragraphNumber, taskNumber, data) {
-
-    var storageData = JSON.parse(localStorage.getItem("userAnswers"));
-
-    if (storageData) {
-        var tasks = storageData.paragraphs[paragraphNumber].tasks[taskNumber];
-        $.each(data, function (key, value) {
-            tasks[key] = value;
-        });
-        storageData.paragraphs[paragraphNumber].tasks[taskNumber] = tasks;
-        localStorage.setItem("userAnswers",  JSON.stringify(storageData));
-        console.log("test Passed Data");
-        var testPassData = JSON.parse(localStorage.getItem("userAnswers"));
-        testPassData = testPassData.paragraphs[paragraphNumber].tasks[taskNumber];
-        $.each(testPassData,function(key,value){
-            console.log(key + " " + value);
-
-        });
-
-    }else{
-
-        var jsonData = {
-            paragraphs: {
-                1: {
-                    tasks: {
-                        1:{
-
-                        }
-                    }
-                }
-            }
-        };
-
-        $.each(data, function (key,value) {
-            jsonData.paragraphs[paragraphNumber].tasks[taskNumber][key] = value;
-        });
-
-        localStorage.setItem("userAnswers", JSON.stringify(jsonData));
-        testPassData = JSON.parse(localStorage.getItem("userAnswers"));
-
-        testPassData = testPassData.paragraphs[paragraphNumber].tasks[taskNumber];
-        console.log("test Passed Data");
-        $.each(testPassData,function(key,value){
-            console.log(key + " " + value);
-
-        });
-
-    }
-
-
-}
-
-function saveData() {
     var answer1 = $('#lab1').text();
     var answer2 = $('#lab2').text();
     var answer3 = $('#lab3').text();
@@ -112,28 +30,69 @@ function saveData() {
     data[answer3] = checkbox3;
     data[answer4] = checkbox4;
 
-    saveObject(1, 1, data);
+    saveToStorage(1, 1, data);
 
-}
+    function saveToStorage(paragraphNumber,taskNumber,data){
 
+        var storageData = JSON.parse(localStorage.getItem("userAnswers"));
 
+        if(storageData) {
+            var tasks = storageData.paragraphs[paragraphNumber].tasks[taskNumber];
+            $.each(data, function (key, value) {
+                tasks[key] = value;
+            });
 
-function getAnswer(paragraphNumber, taskNumber) {
-    var answers = {
-        paragraphs: {
-            1: {
-                tasks: {
+            storageData.paragraphs[paragraphNumber].tasks[taskNumber] = tasks;
+        }else{
+            storageData = {
+                paragraphs: {
                     1: {
-                        165: false,
-                        256: false,
-                        365: true,
-                        335: false
+                        tasks: {
+                            1:{
+
+                            }
+                        }
                     }
                 }
-            }
+            };
+
+            $.each(data, function (key,value) {
+                storageData.paragraphs[paragraphNumber].tasks[taskNumber][key] = value;
+            });
+
+
+
         }
-    };
 
-    return answers.paragraphs[paragraphNumber].tasks[taskNumber];
+        localStorage.setItem("userAnswers", JSON.stringify(storageData));
+    }
+}
 
+function pastDataToThePage(data) {
+    var answer1 = $('#lab1').text();
+    var answer2 = $('#lab2').text();
+    var answer3 = $('#lab3').text();
+    var answer4 = $('#lab4').text();
+
+    $.each(data, function(key, value) {
+        if(answer1 == key){
+            $('#check1').prop('checked', value);
+        }
+        if(answer2 == key){
+            $('#check2').prop('checked', value);
+        }
+        if(answer3 == key){
+            $('#check3').prop('checked', value);
+        }
+        if(answer4 == key){
+            $('#check4').prop('checked', value);
+        }
+    });
+}
+
+function loadData(){
+    var storageData = JSON.parse(localStorage.getItem("userAnswers"));
+
+    var data = storageData.paragraphs[1].tasks[1];
+    pastDataToThePage(data);
 }
